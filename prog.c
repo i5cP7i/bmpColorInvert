e3bfd69f04226239faf5989156539e3c5696633f
i5cP7i <48355782+i5cP7i@users.noreply.github.com>
2019-05-06 15:13:42 +0200

    Update prog.c

--- prog.c
+++ prog.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//input file: read
 #ifndef ifBMP
 #define ifBMP	"C:\\Users\\11703088\\OneDrive - PXL\\Elektronica-ICT\\C_programmeren_2\\PE2\\BMP\\input.bmp"
 #endif
 
+//output file: write
 #ifndef ofBMP
 #define ofBMP	"C:\\Users\\11703088\\OneDrive - PXL\\Elektronica-ICT\\C_programmeren_2\\PE2\\BMP\\output.bmp"	
 #endif
