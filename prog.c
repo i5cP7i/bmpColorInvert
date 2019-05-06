6c2711867963607d334ea9f6851a1994a772b623
i5cP7i <48355782+i5cP7i@users.noreply.github.com>
2019-05-06 19:33:24 +0200

    Update prog.c: functional errors
    The program successfully creates an output file with, but its contents are not correct.

--- prog.c
+++ prog.c
@@ -1,18 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//input file: read
 #ifndef ifBMP
 #define ifBMP	"C:\\Users\\11703088\\OneDrive - PXL\\Elektronica-ICT\\C_programmeren_2\\PE2\\BMP\\input.bmp"
 #endif
 
-//output file: write
 #ifndef ofBMP
 #define ofBMP	"C:\\Users\\11703088\\OneDrive - PXL\\Elektronica-ICT\\C_programmeren_2\\PE2\\BMP\\output.bmp"	
 #endif
 
+typedef unsigned char 	byte;
+typedef unsigned short 	WORD;
+typedef unsigned		DWORD;
+
+typedef struct bmpDataStruct
+{
+	WORD 	magicNum;
+	DWORD 	fileSize;
+	WORD  	rsvd1;        
+	WORD  	rsvd2;        
+	DWORD  	offset;           
+	DWORD 	dib;
+	int 	width;
+	int 	height;
+	WORD  	num_planes;     
+	WORD  	bppx;   
+	DWORD   compType;
+	DWORD 	imgSize;
+	DWORD   x_res; 
+	DWORD   y_res; 
+	DWORD  	num_colors;        
+	DWORD  	important_colors;
+} bmpData;
+
+void fgetData(FILE*, bmpData*); 
+
+const byte headerSize = 54;
+
 int main()
 {
+	bmpData bmp;
 	FILE *ifp = fopen(ifBMP, "rb");
 	
 	if (ifp == NULL)
@@ -24,21 +51,84 @@ int main()
 	
 	byte header[headerSize];
 	
-	fread(header, sizeof(byte), headerSize, ifp);
-	unsigned size = *(unsigned*)&header[2];
-	unsigned width = *(unsigned*)&header[18];
-	unsigned height = *(unsigned*)&header[22];
-	unsigned imgSize = *(unsigned*)&header[34];
-	printf("bmp width = %u\nbmp height = %u\nbmp size = %u bytes\nbmp image size = %u\n", width, height, size, imgSize);
+	printf("\nInput file data:\n");
+	fgetData(ifp, &bmp);
+	printf("bmp Magic number = %4x\nbmp width = %u\nbmp height = %u\nbmp size = %u bytes\nbmp image size = %u\n", 
+		bmp.magicNum, bmp.width, bmp.height, bmp.fileSize, bmp.imgSize);
 	
-	unsigned bmpSize = imgSize;
 	
-	//
-	byte *ipxs = (byte*) calloc(bmpSize, sizeof(byte));
+	DWORD imgSize = bmp.imgSize;
+
+	byte *ipxs = (byte*) calloc(imgSize, sizeof(byte));
 	
-	fread(ipxs, sizeof(byte), bmpSize, ifp);
+	fread(ipxs, sizeof(byte), imgSize, ifp);
 	
 	//close input file
 	fclose(ifp);
+	
+	//Invert the colors
+	for (size_t i = 0; i < (imgSize - 3); i += 3)
+	{
+		byte tmp = ipxs[i];
+		ipxs[i] = ipxs[i+2];
+		ipxs[i+2] = tmp;
+	}
+	
+	//Create output file ofp
+	FILE *ofp = fopen(ofBMP, "wb"); 
+	if (ofp == NULL)
+	{
+		printf("Error while creating output file %s\n", ofBMP);
+		system("pause");
+		exit(EXIT_FAILURE);
+	}
+	//Write bmp header
+	for (size_t i = 0; i < headerSize; ++i)
+	{
+		putc(header[i], ofp);
+	}
+	
+	byte *opxs = (byte*) calloc(imgSize, sizeof(byte));
+	
+	for(size_t i = 0; i < imgSize; ++i)
+	{
+		opxs[i] = 255 - ipxs[i];
+		putc(opxs[i], ofp);
+	}
+	
+	fwrite(ofp, sizeof(byte), imgSize, ofp);
+	puts("Output file generated!");
+	printf("\nOutput file data:\n");
+	fgetData(ofp, &bmp);
+	printf("bmp Magic number = %4x\nbmp width = %u\nbmp height = %u\nbmp size = %u bytes\nbmp image size = %u\n", 
+		bmp.magicNum, bmp.width, bmp.height, bmp.fileSize, bmp.imgSize);
+	fclose(ofp);
+	free(ipxs);
+	free(opxs);
+	
 	return 0;
+}
+
+void fgetData(FILE *ifp_c, bmpData *Data)
+{
+	bmpData *pData = Data;
+	byte header_c[headerSize];
+	fread(header_c, sizeof(byte), headerSize, ifp_c);
+	
+	pData->magicNum = *(WORD*)&header_c[0];							
+	pData->fileSize = *(DWORD*)&header_c[2];                        
+	pData->rsvd1 = *(WORD*)&header_c[6];                                
+	pData->rsvd2 = *(WORD*)&header_c[8];                                                
+	pData->offset = *(DWORD*)&header_c[10];                                                  
+	pData->dib = *(DWORD*)&header_c[14];                            
+	pData->width = *(int*)&header_c[18];                            
+	pData->height = *(int*)&header_c[22];                           
+	pData->num_planes = *(WORD*)&header_c[26];                       
+	pData->bppx = *(WORD*)&header_c[28];                                                   
+	pData->compType = *(DWORD*)&header_c[30];                                                
+	pData->imgSize = *(DWORD*)&header_c[34];                        
+	pData->x_res = *(DWORD*)&header_c[38];                          
+	pData->y_res = *(DWORD*)&header_c[42];                        
+	pData->num_colors = *(DWORD*)&header_c[46];                                              
+	pData->important_colors = *(DWORD*)&header_c[50];                                   
 }
